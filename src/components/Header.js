caaddcbacc3a028c13fa439929f1165fe73837d6
shiccorama <shiccorama@gmail.com>
2022-11-23 23:41:40 +0200

    second_commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
             {/* top header */}
             <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
                     {/* logo */}
-                    <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+                    <div className='mt-2 mx-2 flex items-center flex-grow sm:flex-grow-0'>
                         <img 
                         src="https://links.papareact.com/f90"
                         width={150}
@@ -22,8 +22,24 @@ function Header() {
                     </div>
                     {/* search bar */}
                     <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
-                        <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md' type="text" />
+                        <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text" />
                         <MagnifyingGlassIcon className='p-4 h-12'/>
+                    </div>
+                    {/* three pieces */}
+                    <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+                        <div className='link'>
+                            <p> Hello Sherif Fawzy</p>
+                            <p className='font-extrabold md:text-sm'> Account & Lists </p>
+                        </div>
+                        <div className='link'>
+                            <p> Returns </p>
+                            <p className='font-extrabold md:text-sm'> & Orders </p>
+                        </div>
+                        <div className='link'>
+                            <ShoppingCartIcon  className='h-10'/>
+                            <p className='font-extrabold md:text-sm'> Basket </p>
+                            
+                        </div>
                     </div>
 
             </div>
